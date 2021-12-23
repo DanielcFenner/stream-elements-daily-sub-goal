91b2a7b8ef16fbf3906025e1023764eb61873d55
Daniel Fenner <dcfenner@gmail.com>
2021-12-23 17:48:59 -0500

    Slowly figuring something out with the JS? Don't really know what I'm doing

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const progress = document.querySelector('.progress-done');
 
 progress.style.width = progress.getAttribute('data-done') + '%';
 
+const SUBGOAL = 0;
+
+window.addEventListener('onWidgetLoad', obj => {
+    SUBGOAL = fieldData.subGoal;
+    })
+
+    
+
+
 
