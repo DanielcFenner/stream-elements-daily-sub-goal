d0a10f60e2ef3e3de645faa6c6a582f4e353f09b
Daniel Fenner <85763192+DanielcFenner@users.noreply.github.com>
2021-12-28 16:26:11 -0500

    fixed tab

--- widget.js
+++ widget.js
@@ -4,34 +4,30 @@ let subCount = 0;
 
 window.addEventListener('onWidgetLoad', function (obj) {
   const fieldData = obj.detail.fieldData;
-  subGoal = fieldData.subGoal;    
+  subGoal = fieldData.subGoal;
 });
 
 document.getElementById("sub-count").innerHTML = subCount;
 
 
-    window.addEventListener('onEventReceived', obj => {
-      const listener = obj.detail.listener;
-      const data = obj["detail"]["event"];
-      
-      if (listener === "subscriber-latest") { 
-        subCount += data["amount"]
-        document.getElementById("sub-count").innerHTML = subCount;
-        subPercent = (subCount / subGoal) * 100
-        cssWidth = subPercent + "%";
-        if (subPercent <= 5) {
-          progress.style.height = "40%"
-        } else if (subPercent >= 5 && subPercent <= 10) {
-          progress.style.height = "75%"
-        } else {
-          progress.style.height = "100%"
-        }
-        progress.style.width = cssWidth;
-      }
+window.addEventListener('onEventReceived', obj => {
+  const listener = obj.detail.listener;
+  const data = obj["detail"]["event"];
 
-      });
+  if (listener === "subscriber-latest") {
+    subCount += data["amount"]
+    document.getElementById("sub-count").innerHTML = subCount;
+    subPercent = (subCount / subGoal) * 100
+    cssWidth = subPercent + "%";
+    if (subPercent <= 5) {
+      progress.style.height = "40%"
+    } else if (subPercent >= 5 && subPercent <= 10) {
+      progress.style.height = "75%"
+    } else {
+      progress.style.height = "100%"
+    }
+    progress.style.width = cssWidth;
+  }
 
-      data["subscriber-session"]["count"]
+});
 
-
-      
